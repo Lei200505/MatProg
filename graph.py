30f993956ceb383a3a6068aa1428ad72d8fa8d10
sokparna <ladanyib@icloud.com>
2026-03-27 11:49:04 +0100

    Elso stabil verzio

---
+++ graph.py
@@ -0,0 +1,152 @@
+import numpy as np
+import networkx as nx
+import pandas as pd
+from scipy.spatial import cKDTree
+import json
+import os
+
+class Graph:
+    def __init__(self, source_folder):
+        self.output_folder = source_folder
+        #a grafepitesre az alabbi negy fajlra van szuksegunk amelyet pandas dataframe formatumban fogunk tarolni
+        #stop_times-ban konkret viszonlatonkent (pl 8:15-kor Orsrol indulu M2) a megallo szekvencia
+        #stops ban minden megallo egyeni azonositoja, koordinitai
+        #a tripsben talalhatoak a viszonylatok altalanos osszegyujtese, azaz honnan hova megy es mikor
+        #a routesban talalhatoak a kulonbozo jaratok kulonbozo iranyai
+        self.stop_times = pd.read_csv(f"{source_folder}/stop_times.txt", encoding="UTF-8", low_memory=False)
+        self.stops = pd.read_csv(f"{source_folder}/stops.txt", encoding="UTF-8", low_memory=False)
+        self.trips = pd.read_csv(f"{source_folder}/trips.txt", encoding="UTF-8", low_memory=False)
+        self.routes = pd.read_csv(f"{source_folder}/routes.txt", encoding="UTF-8", low_memory=False)
+
+    def time_from_str(self, times: list):
+        out = []
+        for t in times:
+            h, m, s = map(int, t.split(":"))
+            out.append(3600 * h + 60 * m + s)
+        return out
+
+    def edge_list(self):
+        edges = []
+        #az alabbi metodus ellistat ad amelybol a networkx sajat beepitett graf adatstrukturajat fel tudjuk epiteni
+
+        #a stop_times standard GTFS file
+        #fo informacioja egy konkret viszonylatazonosito (pl: 10:00:00-kor, Puskas fele indulu 75-os trolinak van egy egyedi azonositoja)
+        #megallok egy szekvenciaja, azaz elso megallo: jaszai mari ter, masodik megallo: radnoti miklos utca, stb.
+        #a megallokat technikai azonositokkal melyet self.stops-ban tudunk majd elerni, nevere visszavaltani, illetve azzal meghivni
+        stop_times_sorted = self.stop_times.sort_values(["trip_id", "stop_sequence"])
+
+        for trip_id, group in stop_times_sorted.groupby("trip_id"):
+
+            stops_list = list(group['stop_id'])
+            arr_times = list(self.time_from_str(group["arrival_time"]))
+            dep_times = list(self.time_from_str(group["departure_time"]))
+
+
+            for i in range(len(stops_list)-1):
+                edge = {
+                    "from_stop": stops_list[i],
+                    "to_stop": stops_list[i+1],
+                    "departure_time": dep_times[i],
+                    "arrival_time": arr_times[i+1],
+                    "trip_id": trip_id
+                }
+                edges.append(edge)
+
+        #most benne van: kiindulo megallo, erkezo megallo, erkezes ideje, indulas ideje, illetve a viszonylatszam
+        edges_df = pd.DataFrame(edges)
+
+
+        #a viszonylatszamokhoz kiirjuk a jaratszamot is
+        #pl: 12356ABC egy 7.00-kor indulo hetes busz, ehhez kiirjuk a hetesbusz jaratkodjat
+        edges_df = edges_df.merge(
+            self.trips[["trip_id", "route_id"]],
+            on="trip_id",
+            how="left"
+        )
+
+        #most pedig egy olyan tomb kell nekunk, amiben benne vannak a konkret elek, jaratonkent egy:
+        #1 rekord igy fog kinezni:
+        #kiindulo megallo, erkezesi megallo (konkret uv el), indulasi ido, erkezesi ido parok a nap folyaman, / jaratszam
+        #Szentlelek ter - Florian ter, 237-es busz, [(7:00, 7:05 viszonylat_id), (7:20, 7:25, viszonlat_id),...]
+        edges_collapsed = (
+            edges_df
+            .groupby(["from_stop", "to_stop", "route_id"])
+            .apply(lambda group: list(zip(group["departure_time"], group["arrival_time"], group["trip_id"])))
+            .reset_index(name="departures")
+        )
+
+
+        transfer_edges = self.transfer_edges()
+        if transfer_edges:
+            edges_collapsed = pd.concat(
+                [edges_collapsed, pd.DataFrame(transfer_edges)],
+                ignore_index=True
+            )
+        
+        return edges_collapsed
+
+
+    # https://en.wikipedia.org/wiki/Geographic_coordinate_system
+    def transfer_edges(self, max_transfer_dist = 200, max_walking_speed = 1.5):
+        coords = self.stops[["stop_lat", "stop_lon"]].to_numpy()
+        tree = cKDTree(coords)
+
+        stop_ids = self.stops["stop_id"].to_numpy()
+
+        meters_per_degree = 111000 #ez becsles, wiki: lat: 110.6 km/deg, lon: 111.3 km/deg
+        radius_deg = max_transfer_dist / meters_per_degree
+
+        neighbors_list = tree.query_ball_point(coords, radius_deg)
+        edges = []
+
+        for i, neighbors in enumerate(neighbors_list):
+            neighbors = [j for j in neighbors if j != i]
+
+            if neighbors == []:
+                continue
+
+            dists = np.linalg.norm(coords[neighbors] - coords[i], axis=1) * meters_per_degree
+            travel_times = (dists / max_walking_speed).astype(int)
+            edges.extend([
+                {"from_stop": stop_ids[i],
+                 "to_stop": stop_ids[j],
+                 "route_id": "TRANSFER",
+                 "departures": [(0, t, "TRANSFER")]}
+                for j, t in zip(neighbors, travel_times)
+            ])
+        
+        return edges
+        
+    def graph(self, edgelist):
+        G = nx.MultiDiGraph()
+        for row in edgelist.itertuples(index = False):
+            dep_list = [(int(dep), int(arr - dep)) for dep, arr, _ in row.departures]
+            G.add_edge(
+                row.from_stop,
+                row.to_stop,
+                key=row.route_id,
+                route_id=row.route_id,
+                departures=dep_list
+            )
+        return G
+    
+    def save_graph(self, out_loc=None, fname = "budapest.json"):
+
+        if out_loc is None:
+            out_loc = self.output_folder
+
+        path = os.path.join(out_loc, fname)
+
+        edges = self.edge_list()
+        G = self.graph(edges)
+
+        data = nx.node_link_data(G)
+
+        with open(path, "w", encoding="UTF-8") as f:
+            json.dump(data, f, indent=2)
+        print("File saved successfully at:", path)
+
+source = r"C:\Users\Lenovo\Desktop\matprogcsom\budapest data"
+out_loc = r"C:\Users\Lenovo\Desktop\matprogcsom\grafepites"
+g = Graph(source)
+g.save_graph(out_loc)
