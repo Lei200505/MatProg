42f07962a36f1302f3223bb50aaedbcd48d21258
Lei200505 <leventesimonics@gmail.com>
2026-04-05 11:50:16 +0200

    Egy fájlba írja ki az id-stop párokat

---
+++ stops.py
@@ -0,0 +1,9 @@
+import csv
+
+with open("stops.txt", encoding="utf-8", newline="") as f_in:
+    reader = csv.DictReader(f_in)
+    
+    with open("stops_out.txt", "w", encoding="utf-8") as f_out:
+        for line in reader:
+            f_out.write(f"{line['stop_id']}: {line['stop_name']}\n")
+
